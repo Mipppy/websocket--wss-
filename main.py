d7a81a5310fabb91a2d8b2783bf18a3e2a1ec57a
Mipppy <mippysacc@gmail.com>
2024-05-23 20:36:58 -0400

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ async def handler(websocket, path):
     print(reply)
     await websocket.send(reply)
 
-start_server = websockets.serve(handler, "localhost", 8000)
+start_server = websockets.serve(handler, "0.0.0.0", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
